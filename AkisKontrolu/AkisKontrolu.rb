3b087fcfa0434f6f614dcc53623e6d50134f6c23
mehmetdik <mdik1212@gmail.com>
2015-01-26 02:57:49 +0200

    Added Akis

---
+++ AkisKontrolu/AkisKontrolu.rb
@@ -0,0 +1,20 @@
+#Mantıksal ifadeleri Birleştirme
+true && true || false  #=>true
+true && true && false #=>false
+
+#Not:Mantıksal ifadelerin tersi (!) ile olur
+
+#if-else Yapısı
+
+#eğer tek satır kod yazıcaksak then kullanmalıyız.Tek satırdan fazla satırda yazıcaksak then'e gerek yok
+
+if kosul then puts "Gel" end
+
+#Unless-else Yapısı
+
+#unless if tam tersi bir yapıya sahiptir if'de ifade true ise girer unless'de false ise girer.
+
+#Üçlü Operatör kullanımı
+
+koşul ? ifade1 : ifade2
+#yukarıda koşul true ise ifade1,false ise ifade2 çalışır
