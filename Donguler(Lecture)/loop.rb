221aa4f157d609313ad632f566da9d2039125c6a
mehmetdik <mdik1212@gmail.com>
2015-01-27 20:07:01 +0200

    Create new project(loop)

---
+++ Donguler(Lecture)/loop.rb
@@ -0,0 +1,16 @@
+
+loop do 
+.......	
+end
+
+
+loop {  }
+
+#yukarıda tanımlanan iki şekliylede kullanılır koşullar ise Döngü kontrolleri(break,next..) ile sağlanır
+
+#örnek:
+sayac=1
+loop do
+print "Ruby"
+break if (sayac +=1)>10
+end
